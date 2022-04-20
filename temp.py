18a5b37cffea97a71d8a95393aa4f1fd7150dadb
Sal7one <Alanazi.saleh.77@gmail.com>
2022-04-20 07:41:00 +0300

    hotfix

--- temp.py
+++ temp.py
@@ -2,14 +2,12 @@ from smbus2 import SMBus
 from mlx90614 import MLX90614
 from time import sleep
 
-bus = SMBus(1)
-sensor = MLX90614(bus, address=0x5A)
-temp = sensor.get_object_1()+3.5 # +3.5 for calibration
-
-
 def start():
+    bus = SMBus(1)
+    sensor = MLX90614(bus, address=0x5A)
     attempt = 0
     safe = False
+    temp = sensor.get_object_1()+3.5 # +3.5 for calibration
     while(attempt < 3):
         if(temp > 32.0 and temp < 39.0):
             bus.close()
